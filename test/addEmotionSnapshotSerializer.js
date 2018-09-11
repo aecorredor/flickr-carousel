cbc8ebbbf4a3cce21b4855b2e8f0dd724dac984e
Alejandro Corredor <alejandro@alejandros-mbp.lan>
2018-09-11 01:20:02 -0400

    add jest-emotion custom matchers for styles

--- test/addEmotionSnapshotSerializer.js
+++ test/addEmotionSnapshotSerializer.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import * as emotion from 'emotion';
-import { createSerializer } from 'jest-emotion';
+import { createSerializer, createMatchers } from 'jest-emotion';
 
 // eslint-disable-next-line no-undef
 expect.addSnapshotSerializer(createSerializer(emotion));
+expect.extend(createMatchers(emotion));
